073a41b56e60f788bf170df76b8d4dcc0d2869cc
Murfew <matthew.hurley953@gmail.com>
2024-08-05 09:11:37 -0400

    added basic code for the gameboard and gamecontroller for the console

---
+++ script.js
@@ -0,0 +1,52 @@
+const gameboard = (function () {
+  const board = [];
+  const rows = 3;
+  const columns = 3;
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  // Move validation and applying move
+  // Console printing
+})();
+
+function Cell() {
+  let value = "";
+
+  const getValue = () => value;
+
+  const setValue = (player) => {
+    value = player;
+  };
+}
+
+const gameController = (function (
+  playerOneName = "X Player",
+  playerTwoName = "O Player"
+) {
+  const players = [
+    { name: playerOneName, token: "X" },
+    { name: playerTwoName, token: "O" },
+  ];
+
+  let activePlayer = players[0];
+  const getActivePlayer = () => {
+    activePlayer;
+  };
+
+  function swtichPlayer() {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  }
+
+  function printNewRound() {
+    gameboard.printBoard();
+    console.log(`It's ${getActivePlayer.name}'s turn`);
+  }
+  // Win checking (inside round playing)
+})();
